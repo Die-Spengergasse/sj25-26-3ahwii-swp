8dabf391891cc6cbe4a69a79222178dcc7e2b06c
Georg Graf (SPG) <grafg@spengergasse.at>
2025-10-08 11:16:02 +0200

    seeding

--- 2025-10_deno_prisma_starter/seed.ts
+++ 2025-10_deno_prisma_starter/seed.ts
@@ -1,8 +1,19 @@
-import { PrismaClient } from "prisma/client/";
+import { PrismaClient } from "./prisma/client/client.ts";
 const prisma = new PrismaClient();
 
-export function seed() {}
+const users = ["Ben", "Georgi", "Marcel", "Dejan", "Moritz"];
+
+export async function seed() {
+    for (const user of users) {
+        await prisma.user.create({
+            data: {
+                name: user,
+            },
+        });
+    }
+}
 
 if (import.meta.main) {
-    seed();
+    await seed();
+    console.log("Seeding finished.");
 }
